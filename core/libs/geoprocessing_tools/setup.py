981e940b7c9e2e8c211dcde89170f77b79b4f29d
nsc <nick.schuessler@bgr.de>
2021-09-16 08:42:23 +0200

    Removed unused geoprocessing_tools

--- core/libs/geoprocessing_tools/setup.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from distutils.core import setup
-import setuptools
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize("geoprocessing.pyx"),
-    include_dirs=[numpy.get_include()]
-)
